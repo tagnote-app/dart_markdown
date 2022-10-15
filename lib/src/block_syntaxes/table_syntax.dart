1cf97d0e186929954307e4a81309bdfdd3941efc
chenzhiguang <zhiguang.c@gmail.com>
2022-10-15 14:37:10 +0200

    Update table makers

--- lib/src/block_syntaxes/table_syntax.dart
+++ lib/src/block_syntaxes/table_syntax.dart
@@ -51,8 +51,7 @@ class TableSyntax extends BlockSyntax {
       return null;
     }
 
-    markers.insertAll(0, parsedRow.markers);
-    final head = BlockElement('tableHead', children: [parsedRow.row]);
+    final head = BlockElement('tableHead', children: [parsedRow]);
     parser.advance();
 
     // Advance past the divider of hyphens.
@@ -66,8 +65,7 @@ class TableSyntax extends BlockSyntax {
         expectedColumns: columnCount,
         inTableHead: false,
       );
-      markers.addAll(parsedRow!.markers);
-      rows.add(parsedRow.row);
+      rows.add(parsedRow!);
       parser.advance();
     }
 
@@ -99,7 +97,7 @@ class TableSyntax extends BlockSyntax {
 
   /// Parses a table row at the current line into a table row element, with
   /// parsed table cells.
-  _RowElements? _parseRow(
+  BlockElement? _parseRow(
     SourceSpan content, {
     required bool inTableHead,
     required int expectedColumns,
@@ -203,15 +201,10 @@ class TableSyntax extends BlockSyntax {
       }
     }
 
-    return _RowElements(
-      BlockElement('tableRow', children: rowChildren),
-      markers,
+    return BlockElement(
+      'tableRow',
+      children: rowChildren,
+      markers: markers,
     );
   }
-}
-
-class _RowElements {
-  final BlockElement row;
-  final List<SourceSpan> markers;
-  _RowElements(this.row, this.markers);
 }
